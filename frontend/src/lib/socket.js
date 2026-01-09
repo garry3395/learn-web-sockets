b2633146bdc0cea043b6315d65344f119d30b1a7
garry3395 <g9876430@gmail.com>
2026-01-09 10:46:11 +0100

    final commit

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -4,4 +4,5 @@ import { io } from 'socket.io-client';
 // Change this to your deployed backend URL on Render
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 
-export const socket = io(BACKEND_URL, { withCredentials: true });
+//export const socket = io(https://learn-web-sockets.onrender.com, { withCredentials: true });
+export const socket = io("https://learn-web-sockets.onrender.com", { withCredentials: true });
